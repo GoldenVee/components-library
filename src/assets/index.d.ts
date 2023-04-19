97facc2cf4034079938842cbcecd6e6f51b70358
Aakanksha Mahajan <aakanksha8791@gmail.com>
2023-04-19 12:41:45 -0500

    fix: declare modules for images

---
+++ src/assets/index.d.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '*.png' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.svg' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.jpeg' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.jpg' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.pwebpng' {
+  const value: any;
+  export = value;
+}
